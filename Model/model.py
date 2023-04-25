4b0e22c24e53a1ed18b988f1ab719f33a59d2a41
Ompio <72317376+Ompio@users.noreply.github.com>
2023-04-25 20:25:41 +0200

    płacz i zgrzytanie zębów ale model ma 97% accuracy

--- Model/model.py
+++ Model/model.py
@@ -14,15 +14,17 @@ import random
 import glob
 import matplotlib.pyplot as plt
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
-#%matplotlib inline
+# %matplotlib inline
 
-#physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#print("Num GPUs Available:", len(physical_devices))
-#tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# print("Num GPUs Available:", len(physical_devices))
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-
-os.chdir('C:/Users/crdra/Desktop/BIAI/fer2013')
+os.chdir('..')
+print(os.getcwd())
+os.chdir('fer2013')
 emotions_dirs = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 if os.path.isdir('test/Angry') is False:
     os.makedirs('test/Angry')
@@ -40,38 +42,44 @@ train_path = 'train'
 valid_path = 'validation'
 test_path = 'test'
 
-train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input)\
-    .flow_from_directory(directory=train_path, classes=emotions_dirs, batch_size=10)
-valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input)\
-    .flow_from_directory(directory=valid_path, classes=emotions_dirs, batch_size=10)
-test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input)\
-    .flow_from_directory(directory=test_path, classes=emotions_dirs, batch_size=10, shuffle=False)
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=train_path, target_size=(48, 48), classes=emotions_dirs, batch_size=10)
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=valid_path, target_size=(48, 48), classes=emotions_dirs, batch_size=10)
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=test_path, target_size=(48, 48), classes=emotions_dirs, batch_size=10, shuffle=False)
 
-#print(f"traint b: {train_batches.n}")
+# print(f"traint b: {train_batches.n}")
 
 imgs, labels = next(train_batches);
+
 
 def plotImages(images_arr):
     fig, axes = plt.subplots(1, 10, figsize=(20, 20))
     axes = axes.flatten()
-    for img, ax in zip(images_arr,axes):
+    for img, ax in zip(images_arr, axes):
         ax.imshow(img)
         ax.axis('off')
     plt.tight_layout()
     plt.show()
 
-plotImages(imgs)
+
+#plotImages(imgs)
 print(labels)
 
-model = Sequential ([
-                    Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(48, 48, 3)),
-                    MaxPool2D(pool_size=(2, 2), strides=2),
-                    Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
-                    Flatten(),
-                    Dense(units=7, activation='softmax'),
-                     ])
+model = Sequential([
+    Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(48, 48, 3)),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+    Flatten(),
+    Dense(units=7, activation='softmax'),
+])
 
 model.summary()
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2)
+os.chdir('..')
+model.save('saved_model')
+new_model = tf.keras.models.load_model('saved_model')
+new_model.summary()
