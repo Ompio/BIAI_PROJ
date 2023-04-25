510514b03f6a42dbf588b3122104c7d060ec2f4d
Julia Wojciuch <juliwoj751@student.polsl.pl>
2023-04-25 14:13:57 +0200

    import cd

--- Model/model.py
+++ Model/model.py
@@ -1,1 +1,11 @@
-print("hello")
+from sklearn.metrics import confusion matrix
+import itertools
+import os
+import shutil
+import random
+import glob
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+%matplotlib inline
+
