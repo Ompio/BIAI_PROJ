4d6299f9b7cb7cd80793db1d64595adb903907e8
Ompio <72317376+Ompio@users.noreply.github.com>
2023-04-25 14:13:11 +0200

    AI imports

--- Model/model.py
+++ Model/model.py
@@ -1,1 +1,8 @@
-print("hello")
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
