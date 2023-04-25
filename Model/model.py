0e9b9009c354ee3013ea3cb9389724bd289082fb
Ompio <72317376+Ompio@users.noreply.github.com>
2023-04-25 14:55:37 +0200

    Import Kuby

--- Model/model.py
+++ Model/model.py
@@ -17,8 +17,8 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 #%matplotlib inline
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-print("Num GPUs Available:", len(physical_devices))
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#physical_devices = tf.config.experimental.list_physical_devices('GPU')
+#print("Num GPUs Available:", len(physical_devices))
+#tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
