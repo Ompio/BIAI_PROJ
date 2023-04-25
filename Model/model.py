4212da9a77a1e6b994330a19d24ccdeeafb06d39
Ompio <72317376+Ompio@users.noreply.github.com>
2023-04-25 14:47:06 +0200

    Import Kuby

--- Model/model.py
+++ Model/model.py
@@ -6,7 +6,7 @@ from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D,
 from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
 from keras.preprocessing.image import ImageDataGenerator
-from sklearn.metrics import confusion matrix
+from sklearn.metrics import confusion_matrix
 import itertools
 import os
 import shutil
@@ -15,7 +15,7 @@ import glob
 import matplotlib.pyplot as plt
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-%matplotlib inline
+#%matplotlib inline
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 print("Num GPUs Available:", len(physical_devices))
